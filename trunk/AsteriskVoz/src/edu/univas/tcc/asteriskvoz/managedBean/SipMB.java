8e1f0a526089c3e3fd40a88923ddc7988cfad659
rafaelgleitao@gmail.com <rafaelgleitao@gmail.com@3b62c038-42b1-8126-8363-0a95ee4f3271>
2012-10-24 23:24:51 +0000

    Comando "sip reload" implementado utilizando a api asterisk-java

--- trunk/AsteriskVoz/src/edu/univas/tcc/asteriskvoz/managedBean/SipMB.java
+++ trunk/AsteriskVoz/src/edu/univas/tcc/asteriskvoz/managedBean/SipMB.java
@@ -1,32 +1,24 @@
 package edu.univas.tcc.asteriskvoz.managedBean;
 
+
+
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.util.List;
 
 import javax.ejb.Stateless;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ViewScoped;
 
 import org.asteriskjava.manager.AuthenticationFailedException;
-import org.asteriskjava.manager.EventTimeoutException;
 import org.asteriskjava.manager.ManagerConnection;
 import org.asteriskjava.manager.ManagerConnectionFactory;
-import org.asteriskjava.manager.ManagerConnectionState;
-import org.asteriskjava.manager.ManagerEventListener;
-import org.asteriskjava.manager.ManagerEventListenerProxy;
-import org.asteriskjava.manager.ResponseEvents;
-import org.asteriskjava.manager.SendActionCallback;
 import org.asteriskjava.manager.TimeoutException;
 import org.asteriskjava.manager.action.CommandAction;
-import org.asteriskjava.manager.action.EventGeneratingAction;
-import org.asteriskjava.manager.action.ManagerAction;
-import org.asteriskjava.manager.response.CommandResponse;
-import org.asteriskjava.manager.response.ManagerResponse;
 
 import edu.univas.tcc.asteriskvoz.entity.Sip;
+import edu.univas.tcc.asteriskvoz.exception.AsteriskClientException;
 
 @ManagedBean
 @ViewScoped
@@ -34,8 +26,9 @@ import edu.univas.tcc.asteriskvoz.entity.Sip;
 public class SipMB {
 
 	private Sip registerSip = new Sip();
+	private ManagerConnection managerConnection = null;
 
-	public String registerSip() {
+	public String registerSip() throws AsteriskClientException, TimeoutException {
 
 		File dir = new File("/etc/asterisk");
 		File arq = new File(dir, "sip.conf");
@@ -62,24 +55,31 @@ public class SipMB {
 		} catch (IOException e) {
 			e.printStackTrace();
 		}
-		return null;
-
-		
-	}
-
-	public String sipReload() {
 		
 		
-		ManagerConnectionFactory mfactory = new ManagerConnectionFactory(
-				"localhost", "rafael", "leitao");
-		ManagerConnection ma = mfactory.createManagerConnection();
-
-		CommandAction cmmAct = new CommandAction("sip reload");
 		try {
-			ManagerResponse managerResponse = ma.sendAction(cmmAct);
-			String response = managerResponse.getResponse()
-					+ managerResponse.getMessage();
-			System.out.println(response);
+
+			ManagerConnectionFactory managerConnectionFactory = new ManagerConnectionFactory(
+					"localhost", "manager", "pa55w0rd");
+			managerConnection = managerConnectionFactory
+					.createManagerConnection();
+			managerConnection.login();
+
+		} catch (IllegalStateException e) {
+			throw new AsteriskClientException(
+					"Não foi possível estabelecer uma conexão com o servidor.",
+					e);
+		} catch (IOException e) {
+			throw new AsteriskClientException(
+					"O servidor não foi encontrado no endereço informado.", e);
+		} catch (AuthenticationFailedException e) {
+			throw new AsteriskClientException(
+					"Usuário ou senha inválidos para o servidor localhost.", e);
+		}
+		System.out.println(managerConnection.getState());
+		CommandAction ca = new CommandAction("sip reload");
+		try {
+			managerConnection.sendAction(ca);
 		} catch (IllegalArgumentException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -93,6 +93,12 @@ public class SipMB {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
+		return null;
+
+		
+	}
+
+	public String sipReload() {
 		return null;
 	}
 
